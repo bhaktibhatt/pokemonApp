85efeb5dfcea11503c89d1abdd9201a7d365015a
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-10-14 15:09:11 +0530

    added left side UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './Components/Main';
+import './Components/Styles.css';
+
+function App() {
+  return (
+    <>
+    <Main/>
+    </>
+  );
+}
+
+export default App;
